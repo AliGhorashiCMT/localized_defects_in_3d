ca9c766e8240059d634dae6258f2dd5336e30ea6
Ali Ghorashi <aligho@mit.edu>
2024-10-31 14:35:13 -0400

    julia file for sweeping kpoints for dos calculation

---
+++ fig_dos/dos_sweep.jl
@@ -0,0 +1,17 @@
+task_id, num_tasks = parse.(Int, ARGS)
+println("task_id is $task_id")
+println("num_tasks $num_tasks")
+N = 100
+kpoints = Vector{Float64}[]
+for ky_idx in 1:N
+	ky = -0.5 + ky_idx/N
+	for kz_idx in 1:N	
+		kz = -0.5 + kz_idx/N
+		push!(kpoints, [ky, kz])
+        end
+end
+
+for id in task_id:num_tasks:length(kpoints)
+	ky, kz = kpoints[id]
+	run(`./runlattice.sh $ky $kz $id`)
+end
